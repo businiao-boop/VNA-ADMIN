6d125aa1b6ea41e3b83db6e782568306ae9802e8
Arno <3251136656@qq.com>
2025-05-29 14:32:47 +0800

    feat：登录验证

--- vna-admin-frontend/src/views/login/index.vue
+++ vna-admin-frontend/src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-container">
     <a-card title="登录" class="login-card">
-      <a-form layout="vertical" :model="form">
+      <a-form layout="vertical" :model="form" :rules="rules" ref="formRef">
         <a-form-item label="用户名" name="username">
           <a-input v-model:value="form.username" placeholder="请输入用户名" />
         </a-form-item>
@@ -9,14 +9,14 @@
           <a-input-password
             v-model:value="form.password"
             placeholder="请输入密码"
-            @pressEnter="handleLogin"
+            @pressEnter="handleLogin(formRef)"
           />
         </a-form-item>
         <a-form-item name="rememberMe" :wrapper-col="{ offset: 8, span: 16 }">
           <a-checkbox v-model:value="form.rememberMe">记住我</a-checkbox>
         </a-form-item>
         <a-form-item>
-          <a-button type="primary" @click="handleLogin">登录</a-button>
+          <a-button type="primary" @click="handleLogin(formRef)">登录</a-button>
         </a-form-item>
       </a-form>
     </a-card>
@@ -28,29 +28,47 @@ import { message } from "ant-design-vue";
 import { LoginDto } from "@/types//api/auth";
 import { useUserStore } from "@/stores/user";
 import { useRouter } from "vue-router";
+import type { Rule,FormInstance } from "ant-design-vue/es/form";
 const router = useRouter();
 const userStore = useUserStore();
+const formRef = ref<FormInstance>();
+const rules: Record<string, Rule[]> = {
+  username: [
+    {
+      required: true,
+      message: "请输入用户名",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "请输入密码",
+      trigger: "blur",
+    },
+  ],
+};
 const form = reactive<LoginDto>({
   username: "arno",
   password: "1234",
   rememberMe: true,
 });
 
-const handleLogin = async () => {
-  if (!form.username || !form.password) {
-    return message.warning("请输入用户名和密码");
-  }
-  userStore.login(form).then(
-    () => {
-      userStore.fetchUserInfo().then(() => {
-        message.success("登录成功");
-        router.push("/");
-      });
-    },
-    (err) => {
-      console.log("login", err);
-    }
-  );
+const handleLogin = async (formEl:FormInstance | undefined) => {
+  if(!formEl) return;
+  formEl.validate().then(()=>{
+    userStore.login(form).then(
+      () => {
+        userStore.fetchUserInfo().then(() => {
+          message.success("登录成功");
+          router.push("/");
+        });
+      },
+      (err) => {
+        console.log("login", err);
+      }
+    );
+  })
 };
 </script>
 
